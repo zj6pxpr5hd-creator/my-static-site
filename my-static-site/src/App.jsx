409f9ca91b207158f388debbd9d6a99b540e1bc0
edozzz06 <edoardovolley06@gmail.com>
2026-01-31 14:19:01 +0100

    added about component in main page

--- my-static-site/src/App.jsx
+++ my-static-site/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Hero from './components/Hero.jsx';
+import About from './components/About.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
       <div>
         <Hero />
+        <About />
       </div>
     </>
   )
