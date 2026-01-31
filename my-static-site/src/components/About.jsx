fef448b928e24a67fe382623f6ae1e77e6ead8e8
edozzz06 <edoardovolley06@gmail.com>
2026-01-31 14:17:51 +0100

    added about component, a few changes to Hero component

---
+++ my-static-site/src/components/About.jsx
@@ -0,0 +1,8 @@
+function About() {
+    return (
+        <section>
+            <h2>About me!?</h2>
+            <p>I am a Software Engineering student at the Politecnico of Milan, currently working on my portfolio website.</p>
+        </section>
+    );
+}
--- my-static-site/src/components/Hero.jsx
+++ my-static-site/src/components/Hero.jsx
@@ -3,6 +3,9 @@ function Hero() {
         <div>
             <h1>Edoardo Chessa</h1>
             <p>Software Developer still in the making</p>
+            <p>Welcome to my first static site!</p>
+            <p>My stack at the momenti is Vite+React, deployed on Vercel</p>
+            <p>my portfolio is still a work in progress</p>
         </div>
     );
 }
