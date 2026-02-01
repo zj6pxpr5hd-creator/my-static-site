766215d2ea47a7c68bb5ad1598f0a15a0672d665
edozzz06 <edoardovolley06@gmail.com>
2026-02-01 09:49:37 +0100

    modified Hero Text

--- my-static-site/src/components/Hero.jsx
+++ my-static-site/src/components/Hero.jsx
@@ -5,8 +5,7 @@ function Hero() {
                 <h1>Edoardo Chessa</h1>
                 <p>Software Developer still in the making</p>
                 <p>Welcome to my first static site!</p>
-                <p>My stack at the momenti is Vite+React, deployed on Vercel</p>
-                <p>my portfolio is still a work in progress</p>
+                <p>my portfolio is still a work in progress, but it will be constantly updated with new projects and experiences.</p>
             </div>
         </section>
     );
