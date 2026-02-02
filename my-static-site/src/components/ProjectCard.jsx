7d5e4638be16073469a165d0a76ab5b361e79cb8
edozzz06 <edoardovolley06@gmail.com>
2026-02-02 12:04:13 +0100

    added repo link in project card

--- my-static-site/src/components/ProjectCard.jsx
+++ my-static-site/src/components/ProjectCard.jsx
@@ -1,10 +1,11 @@
-function ProjectCard({ title, description, tech, link }) {
+function ProjectCard({ title, description, tech, link, github }) {
   return (
     <div className="project-card">
       <h3>{title}</h3>
       <p>{description}</p>
       <p className="tech"><strong>Technologies used:</strong> {tech}</p>
-      <a href={link} target="_blank" rel="noreferrer">Learn More</a>
+      <a href={link} target="_blank" rel="noreferrer">Learn More</a><br></br>
+      {github!=="" && <a href={github} target="_blank" rel="noreferrer">See Repo</a>}
     </div>
   );
 }
--- my-static-site/src/components/Projects.jsx
+++ my-static-site/src/components/Projects.jsx
@@ -9,19 +9,22 @@ function Projects() {
                 description="A simple calculator built to practice Javascript fundamentals and DOM manipulation."
                 tech= "html, css, javascript"
                 link= "https://codepen.io/Rouche-Capelli/pen/pvyjrXv"
+                github = ""
             />
             <ProjectCard
                 title = "Tic Tac Toe Game"
                 description = "A classic Tic Tac Toe game implemented in JavaScript with a simple UI."
                 tech= "html, css, javascript"
                 link= "https://codepen.io/Rouche-Capelli/pen/XJdgKWY"
+                github = ""
             />
 
             <ProjectCard
                 title = "Focus Tracker"
-                description = "Simple time tracker to record study sessions. The data gets saved in localStorage and is available between realoads"
+                description = "Simple time tracker to record study sessions. The data gets saved in localStorage and is available between reloads"
                 tech = "React, Vite, Github pages for hosting"
-                link = "https://zj6pxpr5hd-creator.github.io/focus-tracker"  
+                link = "https://zj6pxpr5hd-creator.github.io/focus-tracker"
+                github = "https://github.com/zj6pxpr5hd-creator/focus-tracker.git"
             />
         </section>
     );
